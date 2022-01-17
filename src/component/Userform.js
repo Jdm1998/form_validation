a71c2b8701f9dcd3d8d207ad58a98f4d0fd5ed31
jdmahto1998 <janardanmahto@gmail.com>
2022-01-17 23:30:45 +0530

    completed input form with css

--- src/component/Userform.js
+++ src/component/Userform.js
@@ -1,31 +1,57 @@
 import React from 'react'
-
+import './Userform.css'
 export const Userform = () => {
     return (
-        <div>
+        <div className='userForm'>
             <form>
-                <div>
+
+                <div className='heading'>
+                    User Form
+                </div>
+                <div className='formName'>
                     <label>
                         name
                     </label>
                 </div>
-                <div>
+                <div className='formNameI'>
                     <input type='text'>
 
                     </input>
                 </div>
 
-                <div>
+                <div className='formAge'>
                     <label>
                         Age
                     </label>
                 </div>
-                <div>
+                <div className='formAgeI'>
+                    <input type='number'>
+                        
+                    </input>
+                </div>
+                <div className='formPhn'>
+                    <label>
+                        Phone No
+                    </label>
+                </div>
+                <div className='formPhnI'>
                     <input type='number'>
                         
                     </input>
                 </div>
 
+                <div className='formEmail'>
+                    <label>
+                        Email Address
+                    </label>
+                </div>
+                <div className='formEmailI'>
+                    <input type='text'>
+                        
+                    </input>
+                </div>
+               
+               
                 <div>
                     <button type='submit'>
                         submit
